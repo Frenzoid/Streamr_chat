3946ce094f83f4833a72412a3226d6094237938b
Frenzoid <frenzoid@protonmail.com>
2023-06-25 11:05:08 +0200

    refactored for easier legibility

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,23 +1,73 @@
 
-import { StreamPermission } from "streamr-client";
-import { client } from "../utils/steamr";
-import { useSubscribe } from "streamr-client-react";
+// import { useSubscribe } from "streamr-client-react";
 import { useState, useRef, useEffect } from "react";
 import { PropTypes } from "prop-types";
+
+import { client, checkSubPermissions, checkPubPermissions } from "../utils/steamr";
 import { STREAMR_TOPIC } from "../config/constants";
+
 import Alert from "./Alert";
+
 
 function Chat({ address }) {
 
   const streamRef = useRef(null);
   const [error, setError] = useState(null);
+
   const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+
   const [subPermissions, setSubPermissions] = useState(false);
   const [pubPermissions, setPubPermissions] = useState(false);
-  const [message, setMessage] = useState('');
 
 
   useEffect(() => {
+
+    const loadStream = async () => {
+
+      try {
+
+        // Create or get the stream
+        console.log("Getting the stream...")
+        const stream = await client.getOrCreateStream({
+          id: STREAMR_TOPIC,
+        });
+
+        streamRef.current = stream;
+
+        // Check if user has permission to publish to the stream
+        const hasPubPermissions = await checkSubPermissions(stream, address);
+        console.log('User has permission to publish: ' + hasPubPermissions);
+        setPubPermissions(hasPubPermissions);
+
+        // Check if user has permission to subscribe to the stream
+        const hasSubPermissions = await checkPubPermissions(stream, address);
+        console.log('User has permission to subscribe: ' + hasSubPermissions);
+        setSubPermissions(hasSubPermissions);
+
+        if (hasSubPermissions) {
+
+          // Subscribe to the stream, classic way
+          console.log("Subscribing to the topic...")
+          await client.subscribe(STREAMR_TOPIC, ({ message }) => {
+            setMessages(msgs => [...msgs, message]);
+          });
+
+          // Subscribe to the stream, using the hook
+          /*useSubscribe(STREAMR_TOPIC, {
+            onMessage: (message) => {
+              console.log("New message:", message)
+              setMessages(msgs => [...msgs, message]);
+            },
+          });*/
+        }
+
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+      }
+    }
+
     loadStream();
 
     // Unsubscribe from the stream when component unmounts, IE: when the user disconnects the wallet.
@@ -25,73 +75,26 @@ function Chat({ address }) {
   }, []);
 
 
-  const loadStream = async () => {
-    try {
-      // Create or get the stream
-      const stream = await client.getOrCreateStream({
-        id: STREAMR_TOPIC,
-      });
+  // Publish a message to the stream
+  const sendMessage = async () => {
+    if (message) {
 
-      streamRef.current = stream;
-
-      // Check if user has permission to publish to the stream
-      const hasPubPermissions = await stream.hasPermission({
-        permission: StreamPermission.PUBLISH,
-        user: address,
-        allowPublic: true
-      })
-
-      console.log('User has permission to publish: ' + hasPubPermissions);
-      setPubPermissions(hasPubPermissions);
-
-      // Check if user has permission to subscribe to the stream
-      const hasSubPermissions = await stream.hasPermission({
-        permission: StreamPermission.SUBSCRIBE,
-        user: address,
-        allowPublic: true
-      });
-
-      console.log('User has permission to subscribe: ' + hasSubPermissions);
-      setSubPermissions(hasSubPermissions);
-
-      if (hasSubPermissions) {
-
-        // Subscribe to the stream, classic way
-        console.log("Subscribing to the topic...")
-        client.subscribe(STREAMR_TOPIC, ({ message }) => {
-          setMessages([...messages, ...{ message }]);
-          console.log("New message:", message, messages);
-        });
-
-        // Subscribe to the stream, using the hook
-        /*useSubscribe(STREAMR_TOPIC, {
-          onMessage: (message) => {
-            console.log("New message:", message);
-            setMessages([...messages, message]);
+      try {
+        await streamRef.current.publish({
+          message: {
+            date: new Date().toLocaleString(),
+            sender: address,
+            text: message,
           },
-        });*/
+        });
+        setMessage('');
+        setError(null);
+      } catch (err) {
+        setError(err.message);
       }
 
-    } catch (err) {
-      console.log(err);
-      setError(err.message);
-    }
-  }
-
-  // Publish a message to the stream
-  const sendMessage = () => {
-    if (message) {
-      streamRef.current.publish({
-        message: {
-          date: new Date().toLocaleString(),
-          sender: address,
-          text: message,
-        },
-      }).then(() => {
-        setMessage('');
-      }).catch((err) => {
-        setError(err.message);
-      });
+    } else {
+      setError("Message cannot be empty.");
     }
   }
 
@@ -103,9 +106,9 @@ function Chat({ address }) {
 
   return (
     <div className={"container"}>
-      {error && <Alert message={error} color={"danger"} />}
       <div className="mt-3 col-md-6 offset-md-3">
-        <div className="chat">
+        <div className="chat" style={{ maxHeight: "100px" }}>
+          {messages.length === 0 && <h4 className={"text-center"}>No messages yet :c</h4>}
           {messages.map(({ sender, text, date }, i) => (
             <div key={i} className={"card mb-3"}>
               <div className="card-body d-flex flex-row justify-content-between">
@@ -140,7 +143,8 @@ function Chat({ address }) {
           </div>
         </div>
       </div>
-    </div >
+      {error && <Alert message={error} color={"danger"} />}
+    </div>
   )
 
 }
--- src/utils/steamr.js
+++ src/utils/steamr.js
@@ -1,9 +1,27 @@
 import { StreamrClient } from 'streamr-client';
+import { StreamPermission } from "streamr-client";
 
+// Instantiate StreamrClient with an Ethereum wallet
 const client = new StreamrClient({
   auth: {
     ethereum: window.ethereum,
   },
 });
 
-export { client }
+// Check if the user has permission to publish to a stream
+const checkPubPermissions = async (stream, address) =>
+  await stream.hasPermission({
+    permission: StreamPermission.PUBLISH,
+    user: address,
+    allowPublic: true,
+  });
+
+// Check if the user has permission to subscribe to a stream
+const checkSubPermissions = async (stream, address) =>
+  await stream.hasPermission({
+    permission: StreamPermission.SUBSCRIBE,
+    user: address,
+    allowPublic: true,
+  });
+
+export { client, checkSubPermissions, checkPubPermissions }
